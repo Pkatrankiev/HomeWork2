7598cece13559288a4789cb878f431bc11867e5d
Pkatrankiev <pkatrankiev@abv.bg>
2015-12-16 06:41:48 +0200

    Plus Map

--- app/src/main/java/com/example/siti/homework2/MapActivity.java
+++ app/src/main/java/com/example/siti/homework2/MapActivity.java
@@ -1,24 +1,25 @@
 package com.example.siti.homework2;
 
-import android.location.Address;
-import android.location.Geocoder;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import java.io.IOException;
-import java.util.List;
-
-
 public class MapActivity extends AppCompatActivity {
+    String address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        List<Address> AdresList = null;
-        String location = SecondScreenActyvity.Adres.getText().toString() + " " +
-                SecondScreenActyvity.City.getText().toString();
-        Geocoder geocod = new Geocoder(this);
+
+        address = "geo:0,0?q=" + SecondScreenActyvity.Adres.getText().toString() +
+                " " + SecondScreenActyvity.City.getText().toString();
+
+        Uri gmmIntentUri = Uri.parse(address);
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
 
     }
 
